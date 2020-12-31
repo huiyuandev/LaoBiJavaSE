8142f39e401d5dcd33d5e8864790e725243fc292
hy <2864798982@qq.com>
2020-12-31 09:53:35 +0800

    day12 停止线程

---
+++ src/multithreading/StopThreadDemo.java
@@ -0,0 +1,50 @@
+package multithreading;
+
+class StopThread implements Runnable {
+
+    private boolean flag = true;
+
+    @Override
+    public synchronized void run() {
+        while (flag) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + "......Exception");
+                flag = false;
+            }
+            System.out.println(Thread.currentThread().getName() + "......run");
+        }
+    }
+
+    public void changeFlag() {
+        flag = false;
+    }
+
+
+}
+
+// 停止线程
+public class StopThreadDemo {
+
+    public static void main(String[] args) {
+        StopThread st = new StopThread();
+        Thread t1 = new Thread(st);
+        Thread t2 = new Thread(st);
+        t1.start();
+        t2.start();
+        int num = 0;
+        while (true) {
+            if (num++ == 60) {
+//                st.changeFlag();
+                t1.interrupt();
+                t2.interrupt();
+                break;
+            }
+            System.out.println(Thread.currentThread().getName() + "..." + num);
+        }
+        System.out.println("over");
+    }
+
+
+}
